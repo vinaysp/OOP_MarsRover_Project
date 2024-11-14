073cdb4c0f9639a8bd7c0af84da31f0e64a73a58
Vinay Patel <vinayspatel0@gmail.com>
2024-11-14 17:40:48 +0000

    refactor: class method name to parseRoverInstructions and changed return output name of output object in parseRoverInstructions method to listOfParsedRoverInstructions

--- src/main/java/InputLayer/InstructionParser.java
+++ src/main/java/InputLayer/InstructionParser.java
@@ -1,35 +1,33 @@
 package InputLayer;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class InstructionParser {
 
-    List<InstructionEnum> roverInstruction(String input) throws Exception {
+    List<InstructionEnum> parseRoverInstructions(String input) throws Exception {
 
 
         if( input == null || input.isEmpty()){
             throw new IllegalArgumentException("Invalid Input, input cannot be empty or null");
         }
 
-        List<InstructionEnum> convertInputToEnum = new ArrayList<>();
+        List<InstructionEnum> listOfParsedRoverInstructions = new ArrayList<>();
         String lowercaseInput = input.toLowerCase();
 
         for (int i =0; i < lowercaseInput.length();i++){
             if (lowercaseInput.charAt(i) == 'l') {
-                convertInputToEnum.add(InstructionEnum.L);
+                listOfParsedRoverInstructions.add(InstructionEnum.L);
             } else if (lowercaseInput.charAt(i) == 'r') {
-                convertInputToEnum.add(InstructionEnum.R);
+                listOfParsedRoverInstructions.add(InstructionEnum.R);
             } else if (lowercaseInput.charAt(i) == 'm') {
-                convertInputToEnum.add(InstructionEnum.M);
+                listOfParsedRoverInstructions.add(InstructionEnum.M);
             } else {
                 throw new IllegalArgumentException("Invalid Input please enter enter either: 'l' - to rotate rover left 90 degrees, "+
                 "'r' - rotate rover right 90 degrees "+"or 'm' to move rover forward one space in its current facing direction");
             }
         }
-        return convertInputToEnum;
+        return listOfParsedRoverInstructions;
     }
 
 }
