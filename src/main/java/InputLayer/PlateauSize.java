a13a798df015302360be13f5d436e272d964f78a
Vinay Patel <vinayspatel0@gmail.com>
2024-11-14 22:10:43 +0000

    style: change formate of PlateauSize class

--- src/main/java/InputLayer/PlateauSize.java
+++ src/main/java/InputLayer/PlateauSize.java
@@ -3,9 +3,4 @@ package InputLayer;
 public class PlateauSize {
     int lengthX;
     int widthY;
-
-    int[] confirmPlateauSize (int lengthX, int widthY){
-        return new int[]{lengthX, widthY};
-    }
-
 }
