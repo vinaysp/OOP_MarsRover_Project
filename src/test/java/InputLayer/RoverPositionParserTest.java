5b95ade07dc64ce4f9a3549ba9a5c82a912546db
Vinay Patel <vinayspatel0@gmail.com>
2024-11-15 11:36:47 +0000

    feat: create further test methods - testWithInvalidEmptyStringInput, testWithInvalidNullStringInput, testWithInvalidStringInputGreaterThanLengthThree for RoverPositionParserTest test class

--- src/test/java/InputLayer/RoverPositionParserTest.java
+++ src/test/java/InputLayer/RoverPositionParserTest.java
@@ -34,9 +34,46 @@ class RoverPositionParserTest {
                 () -> assertEquals(expectedResult.getFacing(),actualResult.getFacing())
         );
 
-
     }
 
+    @Test
+    @DisplayName("RoverPositionParser: should return error for an empty or null input string")
+    void testWithInvalidEmptyStringInput() {
+        Exception exception = assertThrows(Exception.class, ()-> {
+            roverPositionParser.instructionsToSetRoverPosition("");
+        });
+
+        String expectedMessage = "Invalid input, instruction must be given as follows : PositionX of rover(number) PositionY of rover(number) CompassDirection of Rover (N,S,E,W) ";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    @DisplayName("RoverPositionParser: should return error for an empty or null input string")
+    void testWithInvalidNullStringInput() {
+        Exception exception = assertThrows(Exception.class, () -> {
+            roverPositionParser.instructionsToSetRoverPosition(null);
+        });
+
+        String expectedMessage = "Invalid input, instruction must be given as follows : PositionX of rover(number) PositionY of rover(number) CompassDirection of Rover (N,S,E,W) ";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    @DisplayName("RoverPositionParser: should return error for an input string of length greater than 3")
+    void testWithInvalidStringInputGreaterThanLengthThree() {
+        Exception exception = assertThrows(Exception.class, () -> {
+            roverPositionParser.instructionsToSetRoverPosition("1234");
+        });
+
+        String expectedMessage = "Invalid input, input cannot be more than three characters";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 
 
 
