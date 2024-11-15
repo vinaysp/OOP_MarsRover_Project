38f5b6e192d5c933e13dafc40dbe925e7677f27e
Vinay Patel <vinayspatel0@gmail.com>
2024-11-15 10:57:32 +0000

    refactor: add getters to RoverPosition class as well as private package constructor to enable testing of class

--- src/main/java/InputLayer/RoverPosition.java
+++ src/main/java/InputLayer/RoverPosition.java
@@ -2,7 +2,24 @@ package InputLayer;
 
 public class RoverPosition {
 
+
     private int roverPositionX;
     private int roverPositionY;
     private CompassDirectionEnum facing;
+
+    RoverPosition(int roverPositionX, int roverPositionY, CompassDirectionEnum compassDirection) {
+    }
+
+
+    int getRoverPositionX() {
+        return roverPositionX;
+    }
+
+    int getRoverPositionY() {
+        return roverPositionY;
+    }
+
+    CompassDirectionEnum getFacing() {
+        return facing;
+    }
 }
