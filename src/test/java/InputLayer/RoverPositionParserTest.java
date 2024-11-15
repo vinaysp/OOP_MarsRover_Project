0155cc95d741ad4b97d6f1057339a3938de95566
Vinay Patel <vinayspatel0@gmail.com>
2024-11-15 09:54:19 +0000

    feat: create testWithValidStringInput test method

--- src/test/java/InputLayer/RoverPositionParserTest.java
+++ src/test/java/InputLayer/RoverPositionParserTest.java
@@ -1,12 +1,39 @@
 package InputLayer;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.text.Position;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoverPositionParserTest {
 
+    RoverPositionParser roverPositionParser;
+
     @BeforeEach
     void setUp() {
+        roverPositionParser = new RoverPositionParser();
     }
+
+    @Test
+    @DisplayName("RoverPositionParser: should take a valid string input and store the input in the correct fields in RoverPosition class  ")
+    void testWithValidStringInput(){
+        //Arrange
+        String input = "12N";
+        RoverPosition expectedResult = new RoverPosition(1,2,CompassDirectionEnum.N);
+
+        //Act
+        RoverPosition actualResult = roverPositionParser.setRoverPosition(input);
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
+
+
+    }
+
+
+
+
 }
