0773447eae4239d7468e5dc5ee3eeaf66341ff2d
Vinay Patel <vinayspatel0@gmail.com>
2024-11-15 22:07:31 +0000

    style: rename rotate method to rotateRover, change wording in method to be more readable

--- src/main/java/LogicLayer/RoverClass.java
+++ src/main/java/LogicLayer/RoverClass.java
@@ -1,5 +1,65 @@
 package LogicLayer;
 
 
+import InputLayer.CompassDirectionEnum;
+import InputLayer.InstructionEnum;
+
+
 public class RoverClass {
+
+
+    public CompassDirectionEnum rotateRover(CompassDirectionEnum startingCompassPositionInput, InstructionEnum inputDirectionChange) {
+
+
+        CompassDirectionEnum finishingCompassPositionOutput = null;
+
+       if (startingCompassPositionInput == CompassDirectionEnum.N) {
+           switch (inputDirectionChange) {
+               case InstructionEnum.L:
+                   finishingCompassPositionOutput = CompassDirectionEnum.W;
+                   break;
+               case InstructionEnum.R:
+                   finishingCompassPositionOutput = CompassDirectionEnum.E;
+                   break;
+           }
+       }
+
+        if (startingCompassPositionInput == CompassDirectionEnum.S) {
+            switch (inputDirectionChange) {
+                case InstructionEnum.L:
+                    finishingCompassPositionOutput = CompassDirectionEnum.E;
+                    break;
+                case InstructionEnum.R:
+                    finishingCompassPositionOutput = CompassDirectionEnum.W;
+                    break;
+            }
+        }
+
+        if (startingCompassPositionInput == CompassDirectionEnum.E) {
+            switch (inputDirectionChange) {
+                case InstructionEnum.L:
+                    finishingCompassPositionOutput = CompassDirectionEnum.N;
+                    break;
+                case InstructionEnum.R:
+                    finishingCompassPositionOutput = CompassDirectionEnum.S;
+                    break;
+            }
+        }
+
+        if (startingCompassPositionInput == CompassDirectionEnum.W) {
+            switch (inputDirectionChange) {
+                case InstructionEnum.L:
+                    finishingCompassPositionOutput = CompassDirectionEnum.S;
+                    break;
+                case InstructionEnum.R:
+                    finishingCompassPositionOutput = CompassDirectionEnum.N;
+                    break;
+            }
+        }
+
+        return finishingCompassPositionOutput;
+
+    }
+
+
 }
